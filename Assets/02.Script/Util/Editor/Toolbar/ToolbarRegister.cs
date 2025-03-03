fb89a11e297ba1482708fc4e2e15c5766864b9e6
kimyir <kimyir2@gmail.com>
2025-03-03 19:30:14 +0900

    씬이동 툴바 기능 추가

---
+++ Assets/02.Script/Util/Editor/Toolbar/ToolbarRegister.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityToolbarExtender;
+
+namespace NKStudio
+{
+    [InitializeOnLoad]
+    public class ToolbarRegister
+    {
+        static ToolbarRegister()
+        {
+            // LeftToolbarGUI는 왼쪽부터 렌더링됩니다.
+            ToolbarExtender.LeftToolbarGUI.Add(SceneSwitchLeftButton.OnToolbarGUI);
+            
+            // RightToolbarGUI는 오른쪽부터 렌더링됩니다.
+            ToolbarExtender.RightToolbarGUI.Add(SceneSwitchRightButton.OnToolbarGUI);
+        }
+    }
+}
